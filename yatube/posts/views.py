c6313777d57eca849d76c31335d4e4e3ccc63886
Boris Pjnamarev <bonapamss@gmail.com>
2023-02-08 11:25:08 +0700

    yatube v1

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,12 +1,26 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
 
 
 def index(request):
-    return HttpResponse('Главная страница')
+    template = 'posts/index.html'
+    title = 'Последние обновления на сайте'
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'title': title,
+        'posts': posts,
+    }
+    return render(request, template, context)
 
 
 def group_posts(request, slug):
-    return HttpResponse(f'Пост {slug}')
+    template = 'posts/group_list.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    title = group.title
+    context = {
+        'title': title,
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
